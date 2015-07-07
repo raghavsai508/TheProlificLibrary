59343cc8a33249f8da05c592f341aad9129a93ce
Raghav Sai Cheedalla <raghavsaicheedalla@Raghavs-MacBook-Pro.local>
2015-07-07 12:22:08 -0700

    Initial Commit

---
+++ TheProlificLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheProlificLibrary
+//
+//  Created by Raghav Sai Cheedalla on 7/7/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheProlificLibrary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheProlificLibrary
+//
+//  Created by Raghav Sai Cheedalla on 7/7/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
